71d43a3ae76fa1a30686c996bf088d6174a21d63
codinggithub2024 <155049872+codinggithub2024@users.noreply.github.com>
2024-08-31 23:10:59 +0800

    Update self_defined_func.py

--- 7.1_plotting/self_defined_func.py
+++ 7.1_plotting/self_defined_func.py
@@ -1,3 +1,12 @@
 def testing(one):
     return one**one
+
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            return False
+        else:
+            return True
+    return False
+    
     
